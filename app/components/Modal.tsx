5d399e1536648c4bc39ce3d9a73abd05a6a0a131
Yami <raphaelribeirocaetano@gmail.com>
2025-06-17 10:39:06 -0300

    updates characters img

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -29,16 +29,23 @@ const Modal = ({isOpen, onClose, character}: ModalProps) => {
   return (
     <div id="modal" className="fixed top-0 left-0 w-full h-screen z-10">
         <div className="flex flex-col justify-center items-center bg-black/50 h-full p-4" onClick={() => {onClose()}}>
-          <div className="relative flex justify-center items-center h-[35rem] mid:w-[55rem] bg-black/80 text-white" >
+          <div className="relative flex justify-center items-center bg-black/80 text-white" >
           <button className='absolute top-0 right-0 text-white text-4xl hover:cursor-pointer' onClick={() => {onClose()}}><MdClose /></button>
           {character && (
-            <div className='flex overflow-hidden justify-center items-center h-full w-full rounded'>
-            <div className='flex max-w-96'>
-            <Image src={character.bigImg} width={300} height={500} alt=''/>
+            <div className='flex flex-col md:flex-row overflow-hidden justify-center items-center h-full w-full rounded'>
+            <div className="relative w-96 max-w-full aspect-square mx-auto">
+              <Image
+                src={character.bigImg}
+                alt={character.alt}
+                fill
+                style={{ objectFit: 'contain' }}
+                sizes="(max-width: 640px) 100vw, 224px"
+                priority
+              />
             </div>
-            <div className='flex flex-col justify-center items-center h-full w-96 p-4 gap-10'>
+            <div className='flex flex-col md:flex-row justify-center items-center h-full w-96 p-4 gap-10'>
               <h1 className='text-3xl'>{character.name.toUpperCase()}</h1>
-              <h1>{character.description}</h1>
+              <h1 className='text-center'>{character.description}</h1>
               <div className='flex flex-col justify-center items-center'>
               <h1>{character.type.toUpperCase()}</h1>
               {renderIcon()}
